f1004e0adcf0cf8c83c9b054b6b4f8bfd3d43eb1
Rifa-K-Malik <malikrif07@gmail.com>
2021-09-30 10:26:00 +0530

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-   //Install express server
-   const express = require('express');
-   const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-   const app = express();
+const app = express();
 
-   // Serve only the static files form the dist directory
-   app.use(express.static(__dirname + '/test-app'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/testapp'));
 
-   app.get('/*', function(req,res) {
-       
-   res.sendFile(path.join(__dirname+'/test-app/index.html'));
-   });
+app.get('/*', function(req,res) {
 
-   // Start the app by listening on the default Heroku port
-   app.listen(process.env.PORT || 8080);
+res.sendFile(path.join(__dirname+'/testapp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
